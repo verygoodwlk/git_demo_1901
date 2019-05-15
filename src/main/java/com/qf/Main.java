9101497f8e0c5688cee7cf85bab99d5953efebea
ken <ken@qq.com>
2019-05-15 15:21:51 +0800

    第一次架构提交

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+/**
+ * @version 1.0
+ * @user ken
+ * @date 2019/5/15 15:16
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git！");
+    }
+}
